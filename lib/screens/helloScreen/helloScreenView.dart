fa7e310754bd89e826269f65d7554939c74f3a53
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-07-24 18:12:13 -0700

    Create HelloScreen scaffold

---
+++ lib/screens/helloScreen/helloScreenView.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HelloScreenView extends StatefulWidget {
+  const HelloScreenView({super.key});
+
+  @override
+  State<HelloScreenView> createState() => _HelloScreenViewState();
+}
+
+class _HelloScreenViewState extends State<HelloScreenView> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea
+    (child:
+     Scaffold(
+
+      
+     ));
+  }
+}
