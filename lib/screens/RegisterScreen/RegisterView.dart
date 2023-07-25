cfefbab1d9870cc4c6f655f9cfa53bae9ac6f584
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-07-24 19:38:00 -0700

    build RegisterPage UI

--- lib/screens/RegisterScreen/RegisterView.dart
+++ lib/screens/RegisterScreen/RegisterView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RegisterView extends StatelessWidget {
   const RegisterView({super.key});
@@ -6,6 +7,71 @@ class RegisterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold());
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 40),
+                
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Welcome to our Registeration :',style: GoogleFonts.anekGurmukhi().copyWith(fontSize: 17),)),
+              ),
+              SizedBox(height: 25,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: GoogleFonts.anekGurmukhi(),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+                    prefixIcon: Icon(Icons.email),
+                    
+                  ),
+                ),
+              ),
+              SizedBox(height: 25,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: GoogleFonts.anekGurmukhi(),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+                    prefixIcon: Icon(Icons.lock),
+                    
+                  ),
+                ),
+              ),
+              SizedBox(height: 25,),
+
+              ElevatedButton(
+                
+                onPressed: (){
+
+              },
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(250, 50),
+                backgroundColor: Color.fromARGB(255, 8, 8, 8)),
+                
+               child: Text('Register',style: GoogleFonts.anekGurmukhi(),)),
+               SizedBox(height: 10,),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Already have an account?..',style: GoogleFonts.anekGurmukhi(),),
+                  GestureDetector(
+                    onTap: (){},
+                    child: Text('Sign in',style: GoogleFonts.anekGurmukhi().copyWith(color: Colors.amber)),)
+
+               ],)
+
+              
+            ],)
+        ,),
+      ));
   }
 }
