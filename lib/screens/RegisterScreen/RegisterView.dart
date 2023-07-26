4bd1ca921d201e40636b8c589cccf818b47e6c77
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-07-26 15:45:34 -0700

    Validate Textfields and create account by tap

--- lib/screens/RegisterScreen/RegisterView.dart
+++ lib/screens/RegisterScreen/RegisterView.dart
@@ -1,77 +1,119 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class RegisterView extends StatelessWidget {
+class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
 
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  String? email;
+  String? password;
+ final GlobalKey<FormState> _globalKey =  GlobalKey();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 40),
-                
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Welcome to our Registeration :',style: GoogleFonts.anekGurmukhi().copyWith(fontSize: 17),)),
-              ),
-              const SizedBox(height: 25,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    hintStyle: GoogleFonts.anekGurmukhi(),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
-                    prefixIcon: const Icon(Icons.email),
-                    
+        body: Form(
+          key: _globalKey ,
+
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 40),
+                  
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text('Welcome to our Registeration :',style: GoogleFonts.anekGurmukhi().copyWith(fontSize: 17),)),
+                ),
+                const SizedBox(height: 25,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    // ignore: body_might_complete_normally_nullable
+                    validator: (value) {
+                      if (value!.isEmpty){
+                        return "can’t take null value ";
+                      }
+                      
+                      
+                    },
+                    onChanged: (value){
+                      email=value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      hintStyle: GoogleFonts.anekGurmukhi(),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+                      prefixIcon: const Icon(Icons.email),
+                      
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 25,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    hintStyle: GoogleFonts.anekGurmukhi(),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
-                    prefixIcon: const Icon(Icons.lock),
-                    
+                const SizedBox(height: 25,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    // ignore: body_might_complete_normally_nullable
+                    validator: (value) {
+                      if(value!.isEmpty){
+                        return 'can’t take null value';
+                      }
+                    },
+                    onChanged: (value) {
+                      password=value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      hintStyle: GoogleFonts.anekGurmukhi(),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+                      prefixIcon: const Icon(Icons.lock),
+                      
+                    ),
                   ),
                 ),
-              ),
-             const  SizedBox(height: 25,),
-
-              ElevatedButton(
+               const  SizedBox(height: 25,),
+        
+                ElevatedButton(
+                  
+                  onPressed: ()async {
+                    if(_globalKey.currentState!.validate()){
+                      
+                        
+                      
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(email:email! , password: password!);
+                    
+                    }
                 
-                onPressed: (){
-
-              },
-              style: ElevatedButton.styleFrom(
-                fixedSize: const Size(250, 50),
-                backgroundColor: const Color.fromARGB(255, 8, 8, 8)),
+        
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(250, 50),
+                  backgroundColor: const Color.fromARGB(255, 8, 8, 8)),
+                  
+                 child: Text('Register',style: GoogleFonts.anekGurmukhi(),)),
+                const  SizedBox(height: 10,),
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Already have an account?..',style: GoogleFonts.anekGurmukhi(),),
+                    GestureDetector(
+                      onTap: (){},
+                      child: Text('Sign in',style: GoogleFonts.anekGurmukhi().copyWith(color: Colors.amber)),)
+        
+                 ],)
+        
                 
-               child: Text('Register',style: GoogleFonts.anekGurmukhi(),)),
-              const  SizedBox(height: 10,),
-               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Already have an account?..',style: GoogleFonts.anekGurmukhi(),),
-                  GestureDetector(
-                    onTap: (){},
-                    child: Text('Sign in',style: GoogleFonts.anekGurmukhi().copyWith(color: Colors.amber)),)
-
-               ],)
-
-              
-            ],)
-        ,),
+              ],)
+          ,),
+        ),
       ));
   }
 }
