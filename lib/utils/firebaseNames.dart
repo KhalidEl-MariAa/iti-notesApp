794614567e9ffec1609596a3cc53dc6a4efb39f8
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-07-29 21:28:26 -0700

    class for firebase collection and fields names

---
+++ lib/utils/firebaseNames.dart
@@ -0,0 +1,10 @@
+abstract class FirebaseNames{
+
+final String collectionNamme='users';
+final String lastNameField='LastName';
+final String firstNameField='firstName';
+final String passfiels='pass';
+final String emailfield='email';
+
+
+}
