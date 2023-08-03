6d3f45f763dc7b4a0b5949752750417aa4658014
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-02 22:21:25 -0700

    add ontap function to delete icon in custom note

--- lib/utils/widgets/customNote.dart
+++ lib/utils/widgets/customNote.dart
@@ -3,9 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 
 // ignore: must_be_immutable
 class CustomNote extends StatelessWidget {
-   CustomNote({super.key,required this.title,required this.content});
+   CustomNote({super.key,required this.title,required this.content,this.ontap});
   String title;
   String content;
+  void Function()? ontap;
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +31,9 @@ class CustomNote extends StatelessWidget {
                   subtitle: Text(content),
                   isThreeLine: true,
                   trailing: Column(
-                    children: [GestureDetector(child:const  Icon(Icons.delete)),
+                    children: [GestureDetector(
+                      onTap:ontap ,
+                      child:const  Icon(Icons.delete)),
                     const SizedBox(height: 5),
                     GestureDetector(child: const Icon(Icons.done))],
                   ) ,
