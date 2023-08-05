e976f1264c4d003db17796d1dd518667739819d7
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-04 19:31:38 -0700

    createScreen for profile add pic and add its route

---
+++ lib/screens/addProfilepicScreen/addProfilepicView.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AddProfilePicView extends StatefulWidget {
+  const AddProfilePicView({super.key});
+
+  @override
+  State<AddProfilePicView> createState() => _AddProfilePicViewState();
+}
+
+class _AddProfilePicViewState extends State<AddProfilePicView> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold());
+  }
+}
